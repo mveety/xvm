6162fc1692f0427235242dca52d066e17ea428e5
Matthew Veety <mveety@gmail.com>
2021-01-10 23:36:50 -0500

    combined overflow code in insts-math. added underflow detection for instructions that need it. added some emacs clutter to the .gitignore

--- cl-arch/insts-math.lisp
+++ cl-arch/insts-math.lisp
@@ -1,14 +1,18 @@
 (in-package :xvm)
 
+(declaim (inline overflow-check))
+(defun overflow-check (cpu n)
+  (if (typep n 'u32int)
+      (setf (flag cpu 9) 0)
+      (setf (flag cpu 9) 1)))
+
 (definstruction add (:args 2 :cycles 1 :use-width nil :use-mode nil)
   (let ((a (register cpu (car args)))
 	(b (register cpu (cadr args)))
 	(tmp 0))
     (setf tmp (+ a b)
 	  (register cpu (car args)) (u32int tmp))
-    (if (typep tmp 'u32int)
-	(setf (flag cpu 9) 0)
-	(setf (flag cpu 9) 1))))
+    (overflow-check cpu tmp)))
 
 (definstruction sub (:args 2 :cycles 1 :use-width nil :use-mode nil)
   (let ((a (register cpu (car args)))
@@ -16,9 +20,9 @@
 	(tmp 0))
     (setf tmp (- a b)
 	  (register cpu (car args)) (u32int tmp))
-    (if (typep tmp 'u32int)
-	(setf (flag cpu 9) 0)
-	(setf (flag cpu 9) 1))))
+    (if (< a b)
+	(setf (flag cpu 9) 1)
+	(overflow-check cpu tmp))))
 
 (definstruction mul (:args 2 :cycles 1 :use-width nil :use-mode nil)
   (let ((a (register cpu (car args)))
@@ -26,9 +30,7 @@
 	(tmp 0))
     (setf tmp (* a b)
 	  (register cpu (car args)) (u32int tmp))
-    (if (typep tmp 'u32int)
-	(setf (flag cpu 9) 0)
-	(setf (flag cpu 9) 1))))
+    (overflow-check cpu tmp)))
 
 (definstruction div (:args 2 :cycles 1 :use-width nil :use-mode nil)
   (let ((a (register cpu (car args)))
@@ -40,12 +42,9 @@
 		(flag cpu 10) 1))
 	(progn
 	  (setf tmp (values (floor a b))
-		(register cpu (car args)) (u32int tmp))
-	  (if (typep tmp 'u32int)
-	      (setf (flag cpu 9) 0
-		    (flag cpu 10) 0)
-	      (setf (flag cpu 9) 1
-		    (flag cpu 10) 0))))))
+		(register cpu (car args)) (u32int tmp)
+		(flag cpu 10) 0)
+	  (overflow-check cpu tmp)))))
 
 (definstruction mod (:args 2 :cycles 1 :use-width nil :use-mode nil)
   (let ((a (register cpu (car args)))
@@ -57,27 +56,24 @@
 		(flag cpu 10) 1))
 	(progn
 	  (setf tmp (mod a b)
-		(register cpu (car args)) (u32int tmp))
-	  (if (typep tmp 'u32int)
-	      (setf (flag cpu 9) 0
-		    (flag cpu 10) 0)
-	      (setf (flag cpu 9) 1
-		    (flag cpu 10) 0))))))
+		(register cpu (car args)) (u32int tmp)
+		(flag cpu 10) 0)
+	  (overflow-check cpu tmp)))))
 
 (definstruction inc (:args 1 :cycles 1 :use-width nil :use-mode nil)
   (let ((a (register cpu (car args)))
 	(tmp 0))
     (setf tmp (+ a 1)
 	  (register cpu (car args)) (u32int tmp))
-    (if (typep tmp 'u32int)
-	(setf (flag cpu 9) 0)
-	(setf (flag cpu 9) 1))))
+    (overflow-check cpu tmp)))
 
 (definstruction dec (:args 1 :cycles 1 :use-width nil :use-mode nil)
   (let ((a (register cpu (car args)))
 	(tmp 0))
-    (setf tmp (- a 1)
-	  (register cpu (car args)) (u32int tmp))
-    (if (typep tmp 'u32int)
-	(setf (flag cpu 9) 0)
-	(setf (flag cpu 9) 1))))
+    (if (eq a 0)
+	(setf (flag cpu 9) 1
+	      (register cpu (car args)) (u32int -1))
+	(progn
+	  (setf tmp (- a 1)
+		(register cpu (car args)) (u32int tmp))
+	  (overflow-check cpu tmp)))))
