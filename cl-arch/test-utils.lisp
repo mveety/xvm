2b20aa6fc8c44f5b933867b6399d680b093d929a
Matthew Veety <mveety@gmail.com>
2021-01-12 03:17:17 -0500

    added a quick and dirty function to quickly check the processor. be careful: doesn't handle interrupts or anything weird.

--- cl-arch/test-utils.lisp
+++ cl-arch/test-utils.lisp
@@ -11,7 +11,6 @@
 
 (defun assemble (start-address strings)
   (let* ((datastream (xvmasm::assemble-strings start-address strings)))
-    (print datastream)
     (load-list-to-memory datastream start-address)))
 
 (defun asm (start-address &rest strings)
@@ -28,3 +27,33 @@
   (let* ((fetch-data (fetch cpu))
 	 (decode-data (decode cpu fetch-data)))
     (exec cpu decode-data)))
+
+;;; quick and dirty function to more easily test the processor
+(defun asm-test (reg-initials reg-results &rest insts)
+  (let ((tcpu (make-cpu))
+	asmres
+	(retval t))
+    (dolist (x reg-initials)
+      (setf (register tcpu (car x)) (cadr x)))
+    (if (equal *memory-map* nil)
+	(initialize-all-memory))
+    (unmap-sysman)
+    (setf (cpu-halted tcpu) nil)
+    (setf asmres (assemble 0 insts))
+    (if (not (equal (getf asmres :size) (getf asmres :written)))
+	(error "unable to assemble code block"))
+    (dotimes (x (length insts))
+      (if (run-cycle tcpu)
+	  (error "interrupt thrown at line ~a" x)))
+    (dolist (x reg-results)
+      (if (equal (register tcpu (car x)) (cadr x))
+	  (format t "passed: r~a = ~a~%"
+		  (car x)
+		  (register tcpu (car x)))
+	  (progn
+	    (format t "failed: r~a = ~a, should be ~a~%"
+		    (car x)
+		    (register tcpu (car x))
+		    (cadr x))
+	    (setf retval nil))))
+    (list retval tcpu)))
